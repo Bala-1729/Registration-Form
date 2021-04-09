63471abce7880668a1daf1e3ab2656900a3850e5
Balasubramaniam A <63883008+Bala-1729@users.noreply.github.com>
2021-04-10 01:16:32 +0530

    initial commit

---
+++ src/com/registration/controller/EmployeeServlet.java
@@ -0,0 +1,56 @@
+package com.registration.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.registration.model.Employee;
+import com.registration.dao.EmployeeDao;
+
+@WebServlet("/register")
+public class EmployeeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private EmployeeDao employeeDao;
+
+    public void init() {
+        employeeDao = new EmployeeDao();
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    	
+    	response.getWriter().append("Served at: ").append(request.getContextPath());
+    	
+    	RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/EmployeeRegistration.jsp");
+    	dispatcher.forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstName = request.getParameter("first_name");
+        String lastName = request.getParameter("last_name");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String address = request.getParameter("address");
+        String contact = request.getParameter("contact");
+
+        Employee employee = new Employee();
+        employee.setFirst_name(firstName);
+        employee.setLast_name(lastName);
+        employee.setUsername(username);
+        employee.setPassword(password);
+        employee.setContact(contact);
+        employee.setAddress(address);
+
+        try {
+            employeeDao.registerEmployee(employee);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        request.getRequestDispatcher("/WEB-INF/views/EmployeeDetails.jsp").forward(request,response);
+    }
+}
